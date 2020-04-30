6117ce35c3450ac5d3aa6b059d38b3f627912823
John Robertson <john.goceljak@gmail.com>
2020-04-30 18:27:47 -0400

    were gaming

--- src/poker.java
+++ src/poker.java
@@ -11,6 +11,7 @@ import javax.swing.border.LineBorder;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JCheckBox;
 import javax.swing.ButtonGroup;
 
@@ -24,6 +25,10 @@ public class poker extends JPanel {
 	private JButton increaseBet;
 	private JButton decreaseBet;
 	private JLabel playerCardDisplay;
+	private JLabel playerCardDisplay2;
+	private JLabel playerCardDisplay3;
+	private JLabel playerCardDisplay4;
+	private JLabel playerCardDisplay5;
 	private JLabel betDisplay;
 	private int totalPlayerChips;
 	private int swapsSelected;
@@ -32,6 +37,10 @@ public class poker extends JPanel {
 	private JCheckBox swapBox3;
 	private JCheckBox swapBox4;
 	private JCheckBox swapBox5;
+	private JButton startButton;
+	private JButton swapButton;
+	private JButton holdButton;
+	private ImageIcon backOfCard;
 	private boolean isGameStarted = false;
 	private boolean swap1 = false;
 	private boolean swap2 = false;
@@ -39,6 +48,8 @@ public class poker extends JPanel {
 	private boolean swap4 = false;
 	private boolean swap5 = false;
 	private boolean swapped = false;
+	private int winMultiplier = -1;
+	private int winnings = 0;
 	
 
 	/**
@@ -56,7 +67,7 @@ public class poker extends JPanel {
 		lblNewLabel_1.setBounds(437, 11, 109, 46);
 		add(lblNewLabel_1);	
 				
-		betDisplay = new JLabel("Bet Amount : 2");
+		betDisplay = new JLabel("Bet Amount : 1");
 		betDisplay.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		betDisplay.setBounds(705, 56, 140, 25);
 		add(betDisplay);
@@ -64,8 +75,8 @@ public class poker extends JPanel {
 		increaseBet = new JButton("+");
 		increaseBet.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(betAmount < 20) {
-					betAmount += 2;
+				if(betAmount < 10) {
+					betAmount += 1;
 					betDisplay.setText("Bet Amount : " + betAmount);
 				}
 			}
@@ -76,8 +87,8 @@ public class poker extends JPanel {
 		decreaseBet = new JButton("-");
 		decreaseBet.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(betAmount > 2) {
-					betAmount -= 2;
+				if(betAmount > 1) {
+					betAmount -= 1;
 					betDisplay.setText("Bet Amount : " + betAmount);
 				}
 			}
@@ -85,8 +96,9 @@ public class poker extends JPanel {
 		decreaseBet.setBounds(855, 56, 46, 23);
 		add(decreaseBet);
 		
-		ImageIcon backOfCard = new ImageIcon("cards/anotherback.png");
-		backOfCard = scaleImage(backOfCard, 131 , 200);		
+		backOfCard = new ImageIcon("cards/anotherback.png");
+		backOfCard = scaleImage(backOfCard, 131 , 200);	
+
 		
 		totalChipDisplay = new JLabel("Total Chips : " + totalPlayerChips);
 		totalChipDisplay.setFont(new Font("Tahoma", Font.PLAIN, 18));
@@ -103,22 +115,25 @@ public class poker extends JPanel {
 		playerCardDisplay.setBackground(Color.WHITE);
 		cardDisplayPanel.add(playerCardDisplay);
 		
+		
+		
 		Panel cardDisplayPanel2 = new Panel();
 		cardDisplayPanel2.setBounds(229, 138, 140, 210);
 		add(cardDisplayPanel2);
 		cardDisplayPanel2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel playerCardDisplay2 = new JLabel();
+		playerCardDisplay2 = new JLabel();
 		playerCardDisplay2.setForeground(Color.WHITE);
 		playerCardDisplay2.setBackground(Color.WHITE);
 		cardDisplayPanel2.add(playerCardDisplay2);
 		
+
 		Panel cardDisplayPanel3 = new Panel();
 		cardDisplayPanel3.setBounds(406, 138, 140, 210);
 		add(cardDisplayPanel3);
 		cardDisplayPanel3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel playerCardDisplay3 = new JLabel();
+		playerCardDisplay3 = new JLabel();
 		playerCardDisplay3.setForeground(Color.WHITE);
 		playerCardDisplay3.setBackground(Color.WHITE);
 		cardDisplayPanel3.add(playerCardDisplay3);
@@ -128,7 +143,7 @@ public class poker extends JPanel {
 		add(cardDisplayPanel4);
 		cardDisplayPanel4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel playerCardDisplay4 = new JLabel();
+		playerCardDisplay4 = new JLabel();
 		playerCardDisplay4.setForeground(Color.WHITE);
 		playerCardDisplay4.setBackground(Color.WHITE);
 		cardDisplayPanel4.add(playerCardDisplay4);
@@ -138,7 +153,7 @@ public class poker extends JPanel {
 		add(cardDisplayPanel5);
 		cardDisplayPanel5.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel playerCardDisplay5 = new JLabel();
+		playerCardDisplay5 = new JLabel();
 		playerCardDisplay5.setForeground(Color.WHITE);
 		playerCardDisplay5.setBackground(Color.WHITE);
 		cardDisplayPanel5.add(playerCardDisplay5);
@@ -349,7 +364,7 @@ public class poker extends JPanel {
 		swapBox5.setBounds(823, 354, 62, 23);
 		add(swapBox5);
 		
-		JButton swapButton = new JButton("Swap");
+		swapButton = new JButton("Swap");
 		swapButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
@@ -399,23 +414,135 @@ public class poker extends JPanel {
 				playerCardDisplay5.setIcon(playerCard5);
 				
                swapButton.setEnabled(false);
-               playerDeck.checkWinConditions(playerDeck);
+               winMultiplier = playerDeck.checkWinConditions(playerDeck);
+               winnings = winMultiplier * betAmount;
+			switch(winMultiplier) {
+			case -1 :					
+				JOptionPane.showMessageDialog(null, "You Lose! Better luck next time!");
+				resetGame();
+				break;
+			case 0 :
+				Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+				JOptionPane.showMessageDialog(null, "One Pair! You recieved your bet back.");
+				
+				resetGame();
+				break;
+			case 1 :
+				Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+				JOptionPane.showMessageDialog(null, "Two Pairs! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+				resetGame();
+				break;
+			case 2 :
+				Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+				JOptionPane.showMessageDialog(null, "Three of a kind! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+				resetGame();
+				break;
+			case 3 :
+				Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+				JOptionPane.showMessageDialog(null, "Straight! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+				resetGame();
+				break;
+			case 5 :
+				Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+				JOptionPane.showMessageDialog(null, "Flush! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+				resetGame();
+				break;
+			case 10 :
+				Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+				JOptionPane.showMessageDialog(null, "Full House! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+				resetGame();
+				break;
+			case 25 :
+				Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+				JOptionPane.showMessageDialog(null, "Four of a kind! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+				resetGame();
+				break;
+			case 100 :
+				Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+				JOptionPane.showMessageDialog(null, "Straight Flush! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+				resetGame();
+				break;
+			case 250 :
+				Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+				JOptionPane.showMessageDialog(null, "Royal Flush! Wow! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+				resetGame();
+				break;
+				
+			}
+               
+               
 			}
 		});
 		swapButton.setBounds(437, 398, 89, 23);
 		add(swapButton);
 		
-		JButton holdButton = new JButton("Swap");
+		holdButton = new JButton("Hold");
 		holdButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				playerDeck.checkWinConditions(playerDeck);
+				winMultiplier = playerDeck.checkWinConditions(playerDeck);
+				if (winMultiplier != -1) {
+				winnings = winMultiplier * betAmount;
+				}
+				switch(winMultiplier) {
+				case -1 :					
+					JOptionPane.showMessageDialog(null, "You Lose! Better luck next time!");
+					resetGame();
+					break;
+				case 0 :
+					Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+					JOptionPane.showMessageDialog(null, "One Pair! You recieved your bet back.");
+					
+					resetGame();
+					break;
+				case 1 :
+					Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+					JOptionPane.showMessageDialog(null, "Two Pairs! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+					resetGame();
+					break;
+				case 2 :
+					Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+					JOptionPane.showMessageDialog(null, "Three of a kind! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+					resetGame();
+					break;
+				case 3 :
+					Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+					JOptionPane.showMessageDialog(null, "Straight! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+					resetGame();
+					break;
+				case 5 :
+					Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+					JOptionPane.showMessageDialog(null, "Flush! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+					resetGame();
+					break;
+				case 10 :
+					Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+					JOptionPane.showMessageDialog(null, "Full House! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+					resetGame();
+					break;
+				case 25 :
+					Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+					JOptionPane.showMessageDialog(null, "Four of a kind! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+					resetGame();
+					break;
+				case 100 :
+					Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+					JOptionPane.showMessageDialog(null, "Straight Flush! You Win! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+					resetGame();
+					break;
+				case 250 :
+					Player.setChips((int) (totalPlayerChips + betAmount + winnings));
+					JOptionPane.showMessageDialog(null, "Royal Flush! Wow! \n\n You gained " + Integer.toString((int) (winnings + betAmount)) + " chips.");
+					resetGame();
+					break;
+					
+				}
 			}
 		});
-		holdButton.setBounds(437, 398, 89, 23);
+		holdButton.setBounds(479, 440, 89, 23);
 		add(holdButton);
 		
 		
-		JButton startButton = new JButton("Start");
+		startButton = new JButton("Start");
 		startButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -426,41 +553,95 @@ public class poker extends JPanel {
 				startButton.setEnabled(false);
 				decreaseBet.setEnabled(false);
 				increaseBet.setEnabled(false);
+				swapButton.setEnabled(true);
+				holdButton.setEnabled(true);
 				
 				mainDeck.populateDeck();
 				mainDeck.shuffle();
 				
 				
-//				ImageIcon playerCard1 = new ImageIcon("cards/" + playerDeck.draw(mainDeck) + ".png");
-				ImageIcon playerCard1 = new ImageIcon("cards/" + playerDeck.draw9() + ".png");
+				ImageIcon playerCard1 = new ImageIcon("cards/" + playerDeck.draw(mainDeck) + ".png");
+//				ImageIcon playerCard1 = new ImageIcon("cards/" + playerDeck.draw9() + ".png");
 				playerCard1 = scaleImage(playerCard1, 131 , 200);		
 				playerCardDisplay.setIcon(playerCard1);
 								
-//				ImageIcon playerCard2 = new ImageIcon("cards/" + playerDeck.draw(mainDeck) + ".png");
-				ImageIcon playerCard2 = new ImageIcon("cards/" + playerDeck.draw9() + ".png");
+				ImageIcon playerCard2 = new ImageIcon("cards/" + playerDeck.draw(mainDeck) + ".png");
+//				ImageIcon playerCard2 = new ImageIcon("cards/" + playerDeck.draw9() + ".png");
 				playerCard2 = scaleImage(playerCard2, 131 , 200);		
 				playerCardDisplay2.setIcon(playerCard2);
 				
-//				ImageIcon playerCard3 = new ImageIcon("cards/" + playerDeck.draw(mainDeck) + ".png");
-				ImageIcon playerCard3 = new ImageIcon("cards/" + playerDeck.draw10() + ".png");
+				ImageIcon playerCard3 = new ImageIcon("cards/" + playerDeck.draw(mainDeck) + ".png");
+//				ImageIcon playerCard3 = new ImageIcon("cards/" + playerDeck.draw10() + ".png");
 				playerCard3 = scaleImage(playerCard3, 131 , 200);		
 				playerCardDisplay3.setIcon(playerCard3);
 				
-//				ImageIcon playerCard4 = new ImageIcon("cards/" + playerDeck.draw(mainDeck) + ".png");
-				ImageIcon playerCard4 = new ImageIcon("cards/" + playerDeck.draw10() + ".png");
+				ImageIcon playerCard4 = new ImageIcon("cards/" + playerDeck.draw(mainDeck) + ".png");
+//				ImageIcon playerCard4 = new ImageIcon("cards/" + playerDeck.draw10() + ".png");
 				playerCard4 = scaleImage(playerCard4, 131 , 200);		
 				playerCardDisplay4.setIcon(playerCard4);
 				
-//				ImageIcon playerCard5 = new ImageIcon("cards/" + playerDeck.draw(mainDeck) + ".png");
-				ImageIcon playerCard5 = new ImageIcon("cards/" + playerDeck.drawK() + ".png");
+				ImageIcon playerCard5 = new ImageIcon("cards/" + playerDeck.draw(mainDeck) + ".png");
+//				ImageIcon playerCard5 = new ImageIcon("cards/" + playerDeck.drawK() + ".png");
 				playerCard5 = scaleImage(playerCard5, 131 , 200);		
 				playerCardDisplay5.setIcon(playerCard5);
 			}
 		});
 		startButton.setBounds(731, 92, 89, 23);
 		add(startButton);
+		
 	}
+	public void resetGame() {
+	startButton.setEnabled(true);
+	decreaseBet.setEnabled(true);
+	increaseBet.setEnabled(true);
+	mainDeck.clearDeck();
+	swapDeck.clearDeck();
+	playerDeck.clearDeck();
+	swapButton.setEnabled(true);
+	holdButton.setEnabled(true);
 	
+	playerCardDisplay.setIcon(backOfCard);
+	playerCardDisplay2.setIcon(backOfCard);
+	playerCardDisplay3.setIcon(backOfCard);
+	playerCardDisplay4.setIcon(backOfCard);
+	playerCardDisplay5.setIcon(backOfCard);
+	
+	swapBox1.setEnabled(true);
+	swapBox2.setEnabled(true);
+	swapBox3.setEnabled(true);
+	swapBox4.setEnabled(true);
+	swapBox5.setEnabled(true);
+	swapBox1.setSelected(false);
+	swapBox2.setSelected(false);
+	swapBox3.setSelected(false);
+	swapBox4.setSelected(false);
+	swapBox5.setSelected(false);
+	
+	swapsSelected = 0;
+	
+	winMultiplier = -1;
+	winnings = 0;
+	betAmount = 1;
+	betDisplay.setText("Bet Amount : " + betAmount);
+	
+	swap1 = false;
+	swap2 = false;
+	swap3 = false;
+	swap4 = false;
+	swap5 = false;
+	swapped = false;
+	
+	refreshChips();
+	
+	
+
+	}
+	public void refreshChips() {
+		System.out.println("refreshed");
+		totalChipDisplay.setText("Total Chips : " + Player.getChips());
+		totalPlayerChips = Player.getChips();
+	}
+
 	private ImageIcon scaleImage(ImageIcon icon, int w, int h){
         int nw = icon.getIconWidth();
         int nh = icon.getIconHeight();
@@ -479,4 +660,5 @@ public class poker extends JPanel {
 
         return new ImageIcon(icon.getImage().getScaledInstance(nw, nh, Image.SCALE_DEFAULT));
     }
+	
 }
